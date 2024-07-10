e9752a29a838419d986df2c4810eba16b68bc368
Nevcan Uludas <nevcanuludas@gmail.com>
2024-07-10 13:49:50 +0400

    Fix, testimonial

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,176 +1,176 @@
 /*
 * ----------------------------------------------------------------------------------------
-Author       : wordpressboss
+Author     : wordpressboss
 Template Name: Creato - Personal Portfolio Template
-Version      : 1.0
+Version    : 1.0
 * ----------------------------------------------------------------------------------------
 */
 
 (function ($) {
-    'use strict';
+  'use strict';
 
-    jQuery(document).ready(function () {
+  jQuery(document).ready(function () {
 
-        /*
-         * ----------------------------------------------------------------------------------------
-         *  PRELOADER JS
-         * ----------------------------------------------------------------------------------------
-         */
+    /*
+     * ----------------------------------------------------------------------------------------
+     *  PRELOADER JS
+     * ----------------------------------------------------------------------------------------
+     */
 
-        var prealoaderOption = $(window);
-        prealoaderOption.on("load", function () {
-            var preloader = jQuery('.preloader');
-            var preloaderArea = jQuery('.preloader-area');
-            preloader.fadeOut();
-            preloaderArea.delay(350).fadeOut('slow');
-        });
-
-
-
-
-        /*
-         * ----------------------------------------------------------------------------------------
-         *  CHANGE MENU BACKGROUND JS
-         * ----------------------------------------------------------------------------------------
-         */
-
-        var headertopoption = $(window);
-        var headTop = $('.header-top-area');
-
-        headertopoption.on('scroll', function () {
-            if (headertopoption.scrollTop() > 400) {
-                headTop.addClass('menu-bg');
-            } else {
-                headTop.removeClass('menu-bg');
-            }
-        });
-
-
-
-        /*
-         * ----------------------------------------------------------------------------------------
-         *  SMOTH SCROOL JS
-         * ----------------------------------------------------------------------------------------
-         */
-
-        $('a.smoth-scroll').on("click", function (e) {
-            var anchor = $(this);
-            $('html, body').stop().animate({
-                scrollTop: $(anchor.attr('href')).offset().top - 50
-            }, 1000);
-            e.preventDefault();
-        });
-
-
-        /*
-         * ----------------------------------------------------------------------------------------
-         *  PARALLAX JS
-         * ----------------------------------------------------------------------------------------
-         */
-
-        var parallaxeffect = $(window);
-        parallaxeffect.stellar({
-            responsive: true,
-            positionProperty: 'position',
-            horizontalScrolling: false
-        });
-
-
-
-        /*
-         * ----------------------------------------------------------------------------------------
-         *  WORK JS
-         * ----------------------------------------------------------------------------------------
-         */
-
-        $('.work-inner').mixItUp();
-
-
-
-        /*
-         * ----------------------------------------------------------------------------------------
-         *  MAGNIFIC POPUP JS
-         * ----------------------------------------------------------------------------------------
-         */
-
-        var magnifPopup = function () {
-            $('.work-popup').magnificPopup({
-                type: 'image',
-                removalDelay: 300,
-                mainClass: 'mfp-with-zoom',
-                gallery: {
-                    enabled: true
-                },
-                zoom: {
-                    enabled: true, // By default it's false, so don't forget to enable it
-
-                    duration: 300, // duration of the effect, in milliseconds
-                    easing: 'ease-in-out', // CSS transition easing function
-
-                    // The "opener" function should return the element from which popup will be zoomed in
-                    // and to which popup will be scaled down
-                    // By defailt it looks for an image tag:
-                    opener: function (openerElement) {
-                        // openerElement is the element on which popup was initialized, in this case its <a> tag
-                        // you don't need to add "opener" option if this code matches your needs, it's defailt one.
-                        return openerElement.is('img') ? openerElement : openerElement.find('img');
-                    }
-                }
-            });
-        };
-        // Call the functions 
-        magnifPopup();
+    var prealoaderOption = $(window);
+    prealoaderOption.on("load", function () {
+      var preloader = jQuery('.preloader');
+      var preloaderArea = jQuery('.preloader-area');
+      preloader.fadeOut();
+      preloaderArea.delay(350).fadeOut('slow');
+    });
 
 
 
 
+    /*
+     * ----------------------------------------------------------------------------------------
+     *  CHANGE MENU BACKGROUND JS
+     * ----------------------------------------------------------------------------------------
+     */
 
-        /*
-         * ----------------------------------------------------------------------------------------
-         *  TESTIMONIAL JS
-         * ----------------------------------------------------------------------------------------
-         */
+    var headertopoption = $(window);
+    var headTop = $('.header-top-area');
 
-        $(".testimonial-list").owlCarousel({
-            items: 1,
-            autoPlay: true,
-            navigation: false,
-            itemsDesktop: [1199, 1],
-            itemsDesktopSmall: [980, 1],
-            itemsTablet: [768, 1],
-            itemsTabletSmall: false,
-            itemsMobile: [479, 1],
-            pagination: true,
-            autoHeight: true,
-        });
+    headertopoption.on('scroll', function () {
+      if (headertopoption.scrollTop() > 400) {
+        headTop.addClass('menu-bg');
+      } else {
+        headTop.removeClass('menu-bg');
+      }
+    });
 
 
-        /*
-         * ----------------------------------------------------------------------------------------
-         *  EXTRA JS
-         * ----------------------------------------------------------------------------------------
-         */
-        $(document).on('click', '.navbar-collapse.in', function (e) {
-            if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
-                $(this).collapse('hide');
-            }
-        });
-        $('body').scrollspy({
-            target: '.navbar-collapse',
-            offset: 195
-        });
+
+    /*
+     * ----------------------------------------------------------------------------------------
+     *  SMOTH SCROOL JS
+     * ----------------------------------------------------------------------------------------
+     */
+
+    $('a.smoth-scroll').on("click", function (e) {
+      var anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $(anchor.attr('href')).offset().top - 50
+      }, 1000);
+      e.preventDefault();
+    });
 
 
-        
-        /*
-         * ----------------------------------------------------------------------------------------
-         *  AJAX JS
-         * ----------------------------------------------------------------------------------------
-         */
+    /*
+     * ----------------------------------------------------------------------------------------
+     *  PARALLAX JS
+     * ----------------------------------------------------------------------------------------
+     */
+
+    var parallaxeffect = $(window);
+    parallaxeffect.stellar({
+      responsive: true,
+      positionProperty: 'position',
+      horizontalScrolling: false
+    });
+
+
+
+    /*
+     * ----------------------------------------------------------------------------------------
+     *  WORK JS
+     * ----------------------------------------------------------------------------------------
+     */
+
+    $('.work-inner').mixItUp();
+
+
+
+    /*
+     * ----------------------------------------------------------------------------------------
+     *  MAGNIFIC POPUP JS
+     * ----------------------------------------------------------------------------------------
+     */
+
+    var magnifPopup = function () {
+      $('.work-popup').magnificPopup({
+        type: 'image',
+        removalDelay: 300,
+        mainClass: 'mfp-with-zoom',
+        gallery: {
+          enabled: true
+        },
+        zoom: {
+          enabled: true, // By default it's false, so don't forget to enable it
+
+          duration: 300, // duration of the effect, in milliseconds
+          easing: 'ease-in-out', // CSS transition easing function
+
+          // The "opener" function should return the element from which popup will be zoomed in
+          // and to which popup will be scaled down
+          // By defailt it looks for an image tag:
+          opener: function (openerElement) {
+            // openerElement is the element on which popup was initialized, in this case its <a> tag
+            // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+            return openerElement.is('img') ? openerElement : openerElement.find('img');
+          }
+        }
+      });
+    };
+    // Call the functions 
+    magnifPopup();
+
+
+
+
+
+    /*
+     * ----------------------------------------------------------------------------------------
+     *  TESTIMONIAL JS
+     * ----------------------------------------------------------------------------------------
+     */
+
+    $(".testimonial-list").owlCarousel({
+      items: 1,
+      autoPlay: true,
+      navigation: false,
+      itemsDesktop: [1199, 1],
+      itemsDesktopSmall: [980, 1],
+      itemsTablet: [768, 1],
+      itemsTabletSmall: false,
+      itemsMobile: [479, 1],
+      pagination: true,
+      autoHeight: true,
+    });
+
+
+    /*
+     * ----------------------------------------------------------------------------------------
+     *  EXTRA JS
+     * ----------------------------------------------------------------------------------------
+     */
+    $(document).on('click', '.navbar-collapse.in', function (e) {
+      if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
+        $(this).collapse('hide');
+      }
+    });
+    $('body').scrollspy({
+      target: '.navbar-collapse',
+      offset: 195
+    });
+
+
+    
+    /*
+     * ----------------------------------------------------------------------------------------
+     *  AJAX JS
+     * ----------------------------------------------------------------------------------------
+     */
 	
 	// Function for email address validation
 	function isValidEmail(emailAddress) {
-        var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+    var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
 
 		return pattern.test(emailAddress);
 
@@ -180,7 +180,7 @@ Version      : 1.0
 		var data = {
 			name: $("#name").val(),
 			email: $("#email").val(),
-            subject: $("#subject").val(),
+      subject: $("#subject").val(),
 			message: $("#message").val()
 		};
 
@@ -201,86 +201,101 @@ Version      : 1.0
 
 		return false;
 	});
-        
+    
 
-        /*
-         * ----------------------------------------------------------------------------------------
-         *  WOW JS
-         * ----------------------------------------------------------------------------------------
-         */
-        new WOW().init();
-
-
-
-
-        /*
-         * ----------------------------------------------------------------------------------------
-         *  TYPE EFFECT JS
-         * ----------------------------------------------------------------------------------------
-         */
-
-        var TxtType = function (el, toRotate, period) {
-            this.toRotate = toRotate;
-            this.el = el;
-            this.loopNum = 0;
-            this.period = parseInt(period, 10) || 1000;
-            this.txt = '';
-            this.tick();
-            this.isDeleting = false;
-        };
-
-        TxtType.prototype.tick = function () {
-            var i = this.loopNum % this.toRotate.length;
-            var fullTxt = this.toRotate[i];
-
-            if (this.isDeleting) {
-                this.txt = fullTxt.substring(0, this.txt.length - 1);
-            } else {
-                this.txt = fullTxt.substring(0, this.txt.length + 1);
-            }
-
-            this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
-
-            var that = this;
-            var delta = 150 - Math.random() * 100;
-
-            if (this.isDeleting) {
-                delta /= 2;
-            }
-
-            if (!this.isDeleting && this.txt === fullTxt) {
-                delta = this.period;
-                this.isDeleting = true;
-            } else if (this.isDeleting && this.txt === '') {
-                this.isDeleting = false;
-                this.loopNum++;
-                delta = 500;
-            }
-
-            setTimeout(function () {
-                that.tick();
-            }, delta);
-        };
-
-        window.onload = function () {
-            var elements = document.getElementsByClassName('typewrite');
-            for (var i = 0; i < elements.length; i++) {
-                var toRotate = elements[i].getAttribute('data-type');
-                var period = elements[i].getAttribute('data-period');
-                if (toRotate) {
-                    new TxtType(elements[i], JSON.parse(toRotate), period);
-                }
-            }
-            // INJECT CSS
-            var css = document.createElement("style");
-            css.type = "text/css";
-            css.innerHTML = ".typewrite > .wrap { border-right: 0.02em solid #fff}";
-            document.body.appendChild(css);
-        };
+    /*
+     * ----------------------------------------------------------------------------------------
+     *  WOW JS
+     * ----------------------------------------------------------------------------------------
+     */
+    new WOW().init();
 
 
 
 
+    /*
+     * ----------------------------------------------------------------------------------------
+     *  TYPE EFFECT JS
+     * ----------------------------------------------------------------------------------------
+     */
+
+    var TxtType = function (el, toRotate, period) {
+      this.toRotate = toRotate;
+      this.el = el;
+      this.loopNum = 0;
+      this.period = parseInt(period, 10) || 1000;
+      this.txt = '';
+      this.tick();
+      this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function () {
+      var i = this.loopNum % this.toRotate.length;
+      var fullTxt = this.toRotate[i];
+
+      if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+      } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+      }
+
+      this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+      var that = this;
+      var delta = 150 - Math.random() * 100;
+
+      if (this.isDeleting) {
+        delta /= 2;
+      }
+
+      if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+      } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+      }
+
+      setTimeout(function () {
+        that.tick();
+      }, delta);
+    };
+
+    window.onload = function () {
+      var elements = document.getElementsByClassName('typewrite');
+      for (var i = 0; i < elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+      }
+      // INJECT CSS
+      var css = document.createElement("style");
+      css.type = "text/css";
+      css.innerHTML = ".typewrite > .wrap { border-right: 0.02em solid #fff}";
+      document.body.appendChild(css);
+    };
+
+
+    /*
+     * ----------------------------------------------------------------------------------------
+     *  NEV JS
+     * ----------------------------------------------------------------------------------------
+     */
+
+    $('body').delay(500).queue(function(){
+      $("#courses").addClass("hidden");
     });
+
+    $("#eduBtn").click(function(e) {
+      $("#courses").toggleClass("hidden");
+      $(this).text(function(i, text){
+        return text === "Show all courses" ? "Hide all courses" : "Show all courses";
+      })
+    });
+
+  });
 
 })(jQuery);

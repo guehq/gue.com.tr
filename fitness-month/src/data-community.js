ec355413fe4416b4fceb3469b9764484e9e1088a
Nevcan Uludas <nevcanuludas@gmail.com>
2025-08-01 13:17:34 +0300

    Add, communities

--- fitness-month/src/data-community.js
+++ fitness-month/src/data-community.js
@@ -21,5 +21,74 @@ window.communities = {
       email: 'nev@gue.com',
       phone: '+905555435144'
     }
+  },
+  'GUE Seattle': {
+    name: 'GUE Seattle',
+    shortName: 'GUE Seattle',
+    description: 'Just a place for GUE Seattle members to share their fitness activities with each other.',
+    motto: '',
+    stravaClubUrl: 'https://www.strava.com/clubs/gue-seattle',
+    website: 'http://www.gue-seattle.org/',
+    logoUrl: 'https://dgalywyr863hv.cloudfront.net/pictures/clubs/935006/20513860/1/large.jpg',
+    country: 'United States',
+    city: 'Seattle, Washington',
+    foundedYear: null,
+    isActive: true,
+    socialMedia: {
+      facebook: '',
+      instagram: '',
+      email: ''
+    },
+    contactPerson: {
+      name: '',
+      email: '',
+      phone: ''
+    }
+  },
+  'GUE Iberia': {
+    name: 'GUE Iberia | Commit to Excellence',
+    shortName: 'GUE Iberia',
+    description: 'Commit to Excellence',
+    motto: 'Commit to Excellence',
+    stravaClubUrl: 'https://www.strava.com/clubs/gue-iberia',
+    website: '',
+    logoUrl: 'https://dgalywyr863hv.cloudfront.net/pictures/clubs/959961/21238904/1/large.jpg',
+    country: 'Spain',
+    city: 'Portugalete, Biscay',
+    foundedYear: null,
+    isActive: true,
+    socialMedia: {
+      facebook: '',
+      instagram: '',
+      email: ''
+    },
+    contactPerson: {
+      name: '',
+      email: '',
+      phone: ''
+    }
+  },
+  'GUE South East Asia': {
+    name: 'GUE South East Asia',
+    shortName: 'GUE South East Asia',
+    description: 'To foster camaraderie and fitness among GUE members in South East Asia',
+    motto: '',
+    stravaClubUrl: 'https://www.strava.com/clubs/guesea',
+    website: '',
+    logoUrl: 'https://dgalywyr863hv.cloudfront.net/pictures/clubs/1275690/32420988/1/large.jpg',
+    country: 'Singapore',
+    city: '',
+    foundedYear: null,
+    isActive: true,
+    socialMedia: {
+      facebook: '',
+      instagram: '',
+      email: ''
+    },
+    contactPerson: {
+      name: '',
+      email: '',
+      phone: ''
+    }
   }
 };

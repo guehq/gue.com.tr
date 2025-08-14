4af6d36b626acdbd5ff93be1ae4265b1b9c4d2cf
Nevcan Uludas <nevcanuludas@gmail.com>
2025-08-14 10:50:23 +0300

    Update, activity data

--- fitness-month/src/config.js
+++ fitness-month/src/config.js
@@ -1,6 +1,6 @@
 const ZAPIER_CSV_PATH = './data/Fithness Month 2025 - AUG.csv';
-const STRAVA_ACCESS_TOKEN = 'd80ced36c69fa7c28484f5588d7cc3fe8b503133';
-const STRAVA_ACCESS_TOKEN_expires_at = '2025-08-13T04:22:08Z';
+const STRAVA_ACCESS_TOKEN = '241848618a3cb4d9130507b0d851435af72bcc98';
+const STRAVA_ACCESS_TOKEN_expires_at = '2025-08-14T12:15:46Z';
 
 function extractDateFromFilename(filename) {
   const match = filename.match(/\d{4}-\d{2}-\d{2}T\d{2}_\d{2}_\d{2}/);

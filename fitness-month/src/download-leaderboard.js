2a39f7e16ebe52b23d185aee6d5dc1f74d10096e
Nevcan Uludas <nevcanuludas@gmail.com>
2025-08-18 18:25:49 +0300

    Fix, jpg styles

--- fitness-month/src/download-leaderboard.js
+++ fitness-month/src/download-leaderboard.js
@@ -34,12 +34,29 @@ function downloadLeaderboardAsJPG(containerId, filename = 'leaderboard.jpg') {
   clone.style.transform = 'scale(1)';
   clone.style.transformOrigin = 'top left';
   // clone.style.width = `${node.offsetWidth}px`;
-  clone.style.width = `2000px`;
+  clone.style.width = '2000px';
   // clone.style.height = `${node.offsetHeight}px`;
-  clone.style.height = `2000px`;
+  clone.style.height = '2000px';
   clone.style.fontSize = '4rem';
   clone.style.imageRendering = 'crisp-edges';
   document.body.appendChild(clone);
+  
+  // SCALE TITLES
+  clone.querySelectorAll('h3').forEach(h3 => {
+    h3.style.fontSize = '6rem';
+    h3.style.margin = '3rem';
+  });
+
+  // SCALE IMAGES
+  clone.querySelectorAll('img').forEach(img => {
+    img.style.transform = `scale(4)`;
+    img.style.transformOrigin = 'center left';
+  });
+
+  // SCALE TEXT
+  clone.querySelectorAll('p').forEach(p => {
+    p.style.marginLeft = '6rem';
+  });
 
   domtoimage.toJpeg(clone, {
     quality: 1,

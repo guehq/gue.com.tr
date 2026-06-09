aefce06a60bdccc1c066d5835f19faa0851b1bea
Nevcan Uludas <nevcanuludas@gmail.com>
2026-06-09 21:55:38 +0300

    REMOVE GUE FITNESS MONTH FILES

--- fitness-month/src/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const burger = document.querySelector('.navbar-burger');
-  const menu = document.getElementById(burger.dataset.target);
-
-  burger.addEventListener('click', function () {
-    burger.classList.toggle('is-active');
-    menu.classList.toggle('is-active');
-  });
-});
